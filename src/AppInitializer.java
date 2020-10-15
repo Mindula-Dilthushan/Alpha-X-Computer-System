7b9883a12b8f68901f7f480156f6a9acd7ddd8ea
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2020-10-15 10:17:47 +0530

    First Commit In Alpha X Com System

---
+++ src/AppInitializer.java
@@ -0,0 +1,19 @@
+//Alpha X Com System
+//Software Design Alpha X Software Solution
+//10-15-20
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"))));
+//      primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,15 @@
+//Alpha-X-Computer-System
+//Software Design Alpha X Software Solution
+//10-15-20
+package controller;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginFormController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
